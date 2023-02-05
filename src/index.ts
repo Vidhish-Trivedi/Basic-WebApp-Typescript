c97c1e78bd5bf554bc67a184ff0948989ee50417
Vidhish-Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-02-05 18:40:38 +0530

    Added Google maps API

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ const user_1 = new User();
 const company_1 = new Company();
 console.log(user_1);
 console.log(company_1);
+
+new google.maps.Map(document.getElementById('map'), {zoom: 1, center: {lat: 0, lng: 0}});
